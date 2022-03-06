120144c317d57dd058e9ccaddf50e83bdc16d639
Rayane <rayanemaciel10@gmail.com>
2022-03-06 00:21:35 -0300

    2 primeiros desafios

---
+++ src/iniciante/WelcomeToJava.java
@@ -0,0 +1,12 @@
+package iniciante;
+
+public class WelcomeToJava {
+    /**O primeiro exercício do Hackerrank é bem simples e solicita
+     * apenas que o desenvolvedor imprima duas linhas de mensagem usando o System.out.println */
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
